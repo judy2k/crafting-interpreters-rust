896eebf073bbdde8bb466d0a97c9b5deca9af2c2
Mark Smith <judy@judy.co.uk>
2025-08-06 16:36:59 +0100

    Functional approach is not great.

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,37 +1,57 @@
-use crate::token::Token;
-use crate::token_type::TokenType::*;
+use std::cell::RefCell;
+use std::string::String;
 
-#[derive(Debug)]
-pub struct Scanner<'a> {
-    source: &'a str,
-    start: usize,
-    current: usize,
-    line: usize,
-    tokens: Vec<Token>,
+use crate::lox::Lox;
+use crate::token::{Token, Value};
+use crate::token_type::TokenType::{self, *};
+
+
+pub fn scan_tokens(lox: &mut Lox, source: &str) -> Vec<Token> {
+    let source: Vec<char> = source.chars().collect();
+    let start: RefCell<usize> = RefCell::new(0);
+    let current: RefCell<usize> = RefCell::new(0);
+    let mut line =  1;
+    let tokens: RefCell<Vec<Token>> = RefCell::new(vec![]);
+
+    let is_at_end = || current.borrow().ge(&source.len());
+    let advance = || {
+        let mut current = current.borrow_mut();
+        let result = source[*current];
+        *current += 1;
+        result
+    };
+    let add_token_literal = |t: TokenType, literal: Value| {
+        let text: String = source[*start.borrow()..*current.borrow()].iter().collect();
+        tokens.borrow_mut().push(Token::new(t, text, literal, line));
+    };
+    let add_token = |t: TokenType| add_token_literal(t, Value::None);
+    
+    let mut scan_token = || {
+        let c = advance();
+        match c {
+            '(' => add_token(LeftParen),
+            ')' => add_token(RightParen),
+            '{' => add_token(LeftBrace),
+            '}' => add_token(RightBrace),
+            ',' => add_token(Comma),
+            '.' => add_token(Dot),
+            '-' => add_token(Minus),
+            '+' => add_token(Plus),
+            ';' => add_token(Semicolon),
+            '*' => add_token(Star),
+            _ => lox.error(line, "Unexpected character."),
+        };
+    };
+    
+    while !is_at_end() {
+        *start.borrow_mut() = *current.borrow();
+        scan_token();
+    }
+
+    tokens.borrow_mut()
+        .push(Token::new(EOF, "".into(), Value::None, line));
+    tokens.take()
 }
 
-impl<'a> Scanner<'a> {
-    pub fn new(source: &'a str) -> Self {
-        Self {
-            source,
-            start: 0,
-            current: 0,
-            line: 1,
-            tokens: Default::default(),
-        }
-    }
 
-    pub fn scan_tokens(mut self) -> Vec<Token> {
-        while (!self.is_at_end()) {
-            self.start = self.current;
-        }
 
-        self.tokens
-            .push(Token::new(EOF, "".into(), None, self.line));
-        self.tokens
-    }
-
-    fn is_at_end(&self) -> bool {
-        self.current >= self.source.len()
-    }
-}
